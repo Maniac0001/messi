8a099129e6f2e5ccea41659d332e354a50515bf8
Maniac0001 <aditya.rokade2021@gmail.com>
2024-12-30 17:15:09 +0530

    sssssssss

---
+++ info.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 1000);
+});
+
+// Get the button
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// Get the height of the hero section
+let heroSection = document.querySelector('.hero'); // Assuming your hero section has the class "hero"
+
+// When the user scrolls down, show the button only if passed the hero section
+window.onscroll = function() {
+    if (window.pageYOffset > heroSection.offsetHeight) {
+        mybutton.classList.add("show");
+    } else {
+        mybutton.classList.remove("show");
+    }
+};
+
+// When the user clicks on the button, scroll to the top
+mybutton.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
